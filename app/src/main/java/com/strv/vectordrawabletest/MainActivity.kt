15ca5d91aa86b3d49315f4682dc7a93b3426ce36
Juraj Kuliska <juraj.kuliska@strv.com>
2018-10-13 20:08:13 +0200

    Support animation restart and cleanup project

--- app/src/main/java/com/strv/vectordrawabletest/MainActivity.kt
+++ app/src/main/java/com/strv/vectordrawabletest/MainActivity.kt
@@ -4,7 +4,6 @@ import android.animation.Animator
 import android.graphics.drawable.AnimatedVectorDrawable
 import android.os.Bundle
 import android.os.Handler
-import android.support.annotation.IdRes
 import android.support.constraint.motion.MotionLayout
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
@@ -16,51 +15,13 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private var isAnimationInProgress = false
-    private var hasCircleToLetterTransitionStarted = false
-
-    private val squareVector: AnimatedVectorDrawable by lazy {
-        resources.getDrawable(
-            R.drawable.anim_morph,
-            theme
-        ) as AnimatedVectorDrawable
-    }
-    private val circleVector: AnimatedVectorDrawable by lazy {
-        resources.getDrawable(
-            R.drawable.anim_morph_reversed,
-            theme
-        ) as AnimatedVectorDrawable
-    }
-    private var isSquare = true
+    private var isAnimationInProgress = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<ImageView>(R.id.image).setOnClickListener {
-            (it as ImageView).setImageDrawable(if (isSquare) squareVector else circleVector)
-            (it.drawable as AnimatedVectorDrawable).start()
-            isSquare = !isSquare
-        }
-        setOnClickListenerForAnimationForView(R.id.letter_v)
-        setOnClickListenerForAnimationForView(R.id.letter_t)
-        setOnClickListenerForAnimationForView(R.id.letter_s)
-        setOnClickListenerForAnimationForView(R.id.letter_r)
-        red_letters_container.setTransitionListener(object : MotionLayout.TransitionListener {
-            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, progress: Float) {
-                if (!hasCircleToLetterTransitionStarted && progress > 0.4) {
-                    hasCircleToLetterTransitionStarted = true
-                    val oneLetterDelay = (resources.getInteger(R.integer.anim_letter_duration) * 0.8).toLong()
-                    animateVectorDrawable(letter_s)
-                    Handler().postDelayed({ animateVectorDrawable(letter_t) }, oneLetterDelay)
-                    Handler().postDelayed({ animateVectorDrawable(letter_r) }, oneLetterDelay * 2)
-                    Handler().postDelayed({ animateVectorDrawable(letter_v) }, oneLetterDelay * 3)
-                    Handler().postDelayed({ circularRevealDarkLogo() }, oneLetterDelay * 5)
-                }
-            }
+        setupTransitionListeners()
 
-            override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {}
-
-        })
         Handler().postDelayed({ red_letters_container.transitionToEnd() }, 50)
     }
 
@@ -72,7 +33,7 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         return when (item?.itemId) {
             R.id.menu_circular_reveal -> {
-                circularRevealDarkLogo()
+                resetToStart()
                 true
             }
             else -> super.onOptionsItemSelected(item)
@@ -80,24 +41,85 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun setOnClickListenerForAnimationForView(@IdRes id: Int) {
-        findViewById<ImageView>(id).setOnClickListener {
-            ((it as ImageView).drawable as AnimatedVectorDrawable).start()
+    private fun resetToStart() {
+        if (!isAnimationInProgress) {
+            isAnimationInProgress = true
+
+
+            red_letters_container.visibility = View.GONE
+            circularRevealDarkLogo(false)
+            //after the circular reveal is gone
+            Handler().postDelayed({
+                loading_overlay.visibility = View.VISIBLE
+                red_letters_container.visibility = View.VISIBLE
+                black_letters_container.visibility = View.VISIBLE
+                red_letters_container.setTransitionListener(null)
+                black_letters_container.setTransitionListener(null)
+                red_letters_container.transitionToStart()
+                black_letters_container.transitionToStart()
+                resetVectorDrawables()
+                Handler().postDelayed({
+                    black_letters_container.visibility = View.INVISIBLE
+                    loading_overlay.visibility = View.GONE
+                    setupTransitionListeners()
+                    red_letters_container.transitionToEnd()
+                }, 3000)
+            }, 500)
         }
+    }
+
+    private fun setupTransitionListeners() {
+        red_letters_container.setTransitionListener(object : MotionLayout.TransitionListener {
+            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, progress: Float) {}
+
+            override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
+                val oneLetterDelay = (resources.getInteger(R.integer.anim_letter_duration) * 0.8).toLong()
+                animateVectorDrawable(letter_s)
+                Handler().postDelayed({ animateVectorDrawable(letter_t) }, oneLetterDelay)
+                Handler().postDelayed({ animateVectorDrawable(letter_r) }, oneLetterDelay * 2)
+                Handler().postDelayed({ animateVectorDrawable(letter_v) }, oneLetterDelay * 3)
+                Handler().postDelayed({ circularRevealDarkLogo(true) }, oneLetterDelay * 5)
+            }
+
+        })
+
+        black_letters_container.setTransitionListener(object : MotionLayout.TransitionListener {
+            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {}
+
+            override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
+                isAnimationInProgress = false
+            }
+        })
     }
 
     private fun animateVectorDrawable(imageView: ImageView) {
         (imageView.drawable as AnimatedVectorDrawable).start()
     }
 
-    private fun circularRevealDarkLogo() {
-        val containerView = findViewById<View>(R.id.black_letters_container)
+    private fun resetVectorDrawable(imageView: ImageView) {
+        (imageView.drawable as AnimatedVectorDrawable).reset()
+    }
+
+    private fun resetVectorDrawables() {
+        resetVectorDrawable(letter_s)
+        resetVectorDrawable(letter_s1)
+        resetVectorDrawable(letter_t)
+        resetVectorDrawable(letter_t1)
+        resetVectorDrawable(letter_r)
+        resetVectorDrawable(letter_r1)
+        resetVectorDrawable(letter_v)
+        resetVectorDrawable(letter_v1)
+    }
+
+
+    private fun circularRevealDarkLogo(reveal: Boolean) {
+        val containerView = black_letters_container
         val animator: Animator = ViewAnimationUtils.createCircularReveal(
             containerView,
             containerView.width / 2,
             containerView.height,
-            0f,
-            containerView.height * 1.5f
+            if (reveal) 0f else containerView.height * 1.5f,
+            if (reveal) containerView.height * 1.5f else 0f
         )
         containerView.visibility = View.VISIBLE
         animator.start()
@@ -106,7 +128,11 @@ class MainActivity : AppCompatActivity() {
             override fun onAnimationCancel(p0: Animator?) {}
             override fun onAnimationStart(p0: Animator?) {}
             override fun onAnimationEnd(p0: Animator?) {
-                Handler().postDelayed({ animateLettersToSquares() }, 300)
+                if (!reveal) {
+                    containerView.visibility = View.INVISIBLE
+                } else {
+                    Handler().postDelayed({ animateLettersToSquares() }, 300)
+                }
             }
         })
     }
